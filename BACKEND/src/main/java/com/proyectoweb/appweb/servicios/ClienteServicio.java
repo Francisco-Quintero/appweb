398af7259835995a2e09efe32f25784ae4aafdf3
Francisco Quintero <franciscoquintero779@gmail.com>
2024-11-15 22:35:27 -0500

    Configuración preliminar para preparar la DB

--- BACKEND/src/main/java/com/proyectoweb/appweb/servicios/ClienteServicio.java
+++ BACKEND/src/main/java/com/proyectoweb/appweb/servicios/ClienteServicio.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.proyectoweb.appweb.entidades.Cliente;
 import com.proyectoweb.appweb.repositorio.ClienteRepositorio;
 
